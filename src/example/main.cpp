48e027a21bf6650e65311664c97d997ead36a41a
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2024-03-03 16:40:37 +0300

    Removed iostream package usage

--- src/example/main.cpp
+++ src/example/main.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
+#include <string>
 #include "../lib/sklaga.h"
 
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
 
 int main() {
-    std::cout << "Hello" << std::endl;
     init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "sklaga example");
 }
